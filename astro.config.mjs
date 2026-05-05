054298b8396fd6f70e19dff609dcf246221c5483
parazok <parazok.yadpa@gmail.com>
2026-05-05 16:27:03 +0330

    added preact

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,10 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import preact from "@astrojs/preact";
+
 // https://astro.build/config
 export default defineConfig({
-    site: "https://doggydev.ir"
+  site: "https://doggydev.ir",
+  integrations: [preact()]
 });
